c2e081813d75c037977d79f1bb0af8a99fe16a85
Jaiden Groenland <94353701+JaidenGroenland@users.noreply.github.com>
2022-11-11 09:47:08 +0100

    safe

---
+++ overons.php
@@ -0,0 +1,79 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/index.css">
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <title>sv groenland</title>
+</head>
+<body>
+<div class="container-fluid">
+    <nav class="navbar navbar-expand-lg sticky-top bg-primary">
+        <div class="container-fluid">
+            <a class="navbar-brand text-light" href="#">
+                <img src="img/voetbal.png" alt="Logo" width="70" height="auto" class="d-inline-block align-text-top"></a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link text-light fs-4 col-1 ps-4" aria-current="page" href="index.php">home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link active text-light fs-4 col-1 ps-4" aria-current="page" href="overons.php"><u>S.V.GROENLAND</u></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light fs-4 col-1" href="aanmelden.php">aanmelden</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <br>
+    <br>
+    <!--here is the text-->
+    <div class="container-fluid p-0">
+        <img src="img/voetbal-banner.jpg" class="pb-5" alt="banner" width="100%">
+
+
+
+        <footer class="container-fluid bg-primary">
+    <div class="container text-white">
+        <div class="row text-center fs-15">
+            <!--the first col with contact gegevens-->
+            <div class="col-6">
+                <div class="text-white">
+                    <p>
+                    <h5>  Contactgegevens</h5>
+                    sv groenland <br>
+                    citroenplantsoen 41 <br>
+                    2552QS <br>
+                    070-3978929<br>
+                    </p>
+                </div>
+            </div>
+            <!--the second col with openingstijden-->
+            <div class="col-6">
+                <div class="text-white">
+                    <p>
+                    <h5>  teams</h5>
+                    Jo9 <br>
+                    Jo16 <br>
+                    Jo18 <br>
+                    1e  <br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</div>
+</div>
+</body>
+</html>
